afb31ffb372c1fa24be9621013dff8facab4a12d
Pranav Sivaraman <pranavsivaraman@gmail.com>
2024-09-03 18:26:10 -0400

    tests: fix tail_fib add some more observatory comments

--- test.cpp
+++ test.cpp
@@ -6,8 +6,9 @@
  * - Multiple Return Statements?
  */
 
-// Huh. this just gets optimized into the tail call?
-// which then does not get inlined
+// Huh. this just gets optimized into a tail call?
+// which then does not get inlined. Is there a specific
+// pass for fibonacci?
 int fib(int x) {
   if (x == 0 || x == 1) {
     return x;
@@ -17,11 +18,18 @@ int fib(int x) {
 }
 
 // This function gets inlined
-int tail_fib(int x, int acc = 1) {
-  if (x == 0 || x == 1) {
-    return acc;
+// I guess there is pass that converts this to a loop? Looking at the IR
+// there is.
+int tail_fib(int x, int a = 0, int b = 1) {
+  if (x == 0) {
+    return a;
   }
-  return tail_fib(x - 1, acc * x);
+
+  if (x == 1) {
+    return b;
+  }
+
+  return tail_fib(x - 1, b, a + b);
 }
 
 void foo(int N) {
